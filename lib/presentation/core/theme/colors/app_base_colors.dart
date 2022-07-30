93c2a5feae198fb1a1bfbef1d4ec4062e1a59a7d
Gabriel <gcostaapps@gmail.com>
2022-07-30 15:05:40 -0300

    Adding custom theme

---
+++ lib/presentation/core/theme/colors/app_base_colors.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+
+class AppBaseColors {
+  //Grayscale Colors
+  static const Color offBlack = Color(0xFF14142B);
+  static const Color ash = Color(0xFF262338);
+  static const Color body = Color(0xFF4E4B66);
+  static const Color label = Color(0xFF6E7191);
+  static const Color placeholder = Color(0xFFA0A3BD);
+  static const Color line = Color(0xFFD9DBE9);
+  static const Color input = Color(0xFFEFF0F6);
+  static const Color background = Color(0xFFF7F7FC);
+  static const Color offWhite = Color(0xFFFCFCFC);
+
+  static MaterialColor errorColor = const MaterialColor(0xFFED2E3E, {
+    50: Color(0xFFFFF3F8),
+    100: Color(0xFFFBE9E9),
+    200: Color(0xFFFF9999),
+    300: Color(0xFFFF0000),
+    400: Color(0xFFB8004E),
+  });
+
+  static MaterialColor successColor = const MaterialColor(0xFF00BA88, {
+    50: Color(0xFFF2FFFB),
+    100: Color(0xFFE7FCF6),
+    200: Color(0xFF34EAB9),
+    300: Color(0xFF00BA88),
+    400: Color(0xFF00805D),
+  });
+
+  static MaterialColor warningColor = const MaterialColor(0xFFF4B740, {
+    50: Color(0xFFFFF9EF),
+    100: Color(0xFFFFF6E4),
+    200: Color(0xFFFFDB94),
+    300: Color(0xFFF4B740),
+    400: Color(0xFF946200),
+  });
+
+  static const MaterialColor lightTextColors = MaterialColor(
+    0xFFFCFCFC,
+    {
+      50: Color(0xFF14142B),
+      100: Color(0xFF262338),
+      200: Color(0xFF4E4B66),
+      300: Color(0xFF6E7191),
+      400: Color(0xFFA0A3BD),
+      500: Color(0xFFD9DBE9),
+      600: Color(0xFFEFF0F6),
+      700: Color(0xFFF7F7FC),
+      800: Color(0xFFFCFCFC),
+    },
+  );
+
+  static const MaterialColor darkTextColors = MaterialColor(
+    0xFF14142B,
+    {
+      800: Color(0xFF14142B),
+      700: Color(0xFF262338),
+      600: Color(0xFF4E4B66),
+      500: Color(0xFF6E7191),
+      400: Color(0xFFA0A3BD),
+      300: Color(0xFFD9DBE9),
+      200: Color(0xFFEFF0F6),
+      100: Color(0xFFF7F7FC),
+      50: Color(0xFFFCFCFC),
+    },
+  );
+
+  static const MaterialColor lightSurfaceColors = MaterialColor(
+    0xFFFCFCFC,
+    {
+      50: Color(0xFFFCFCFC),
+      100: Color(0xFFF7F7FC),
+      200: Color(0xFFEFF0F6),
+    },
+  );
+
+  static const MaterialColor darkSurfaceColors = MaterialColor(
+    0x7F2F3565,
+    {
+      50: Color(0xFF39394C),
+      100: Color(0xFF202035),
+      200: Color(0xFF202035),
+      300: Color(0x7F2F3565),
+    },
+  );
+
+  static const MaterialColor chili = MaterialColor(
+    0xFFFF0000,
+    chiliSwatch,
+  );
+
+  static const Map<int, Color> chiliSwatch = {
+    50: Color(0xFFFFF5F5),
+    100: Color(0xFFFFDBDB),
+    200: Color(0xFFFF9999),
+    300: Color(0xFFFF6666),
+    400: Color(0xFFFF4C4D),
+    500: Color(0xFFFF0000),
+    600: Color(0xFFE50000),
+    700: Color(0xFFCC0000),
+    800: Color(0xFF990000),
+    900: Color(0xFF660000),
+  };
+}
---
+++ lib/presentation/core/theme/colors/app_colors.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+import 'app_base_colors.dart';
+
+class AppColors {
+  final bool isLight;
+
+  AppColors(this.isLight);
+
+  Color get surfaceColor50 => isLight
+      ? AppBaseColors.lightSurfaceColors.shade50
+      : AppBaseColors.darkSurfaceColors.shade50;
+
+  Color get surfaceColor100 => isLight
+      ? AppBaseColors.lightSurfaceColors.shade100
+      : AppBaseColors.darkSurfaceColors.shade100;
+
+  Color get surfaceColor200 => isLight
+      ? AppBaseColors.lightSurfaceColors.shade200
+      : AppBaseColors.darkSurfaceColors.shade200;
+
+  Color get textColor50 => isLight
+      ? AppBaseColors.lightTextColors.shade50
+      : AppBaseColors.darkTextColors.shade50;
+
+  Color get textColor100 => isLight
+      ? AppBaseColors.lightTextColors.shade100
+      : AppBaseColors.darkTextColors.shade100;
+
+  Color get textColor200 => isLight
+      ? AppBaseColors.lightTextColors.shade200
+      : AppBaseColors.darkTextColors.shade200;
+
+  Color get textColor300 => isLight
+      ? AppBaseColors.lightTextColors.shade300
+      : AppBaseColors.darkTextColors.shade300;
+
+  Color get textColor400 => isLight
+      ? AppBaseColors.lightTextColors.shade400
+      : AppBaseColors.darkTextColors.shade400;
+
+  Color get textColor500 => isLight
+      ? AppBaseColors.lightTextColors.shade500
+      : AppBaseColors.darkTextColors.shade500;
+
+  Color get textColor600 => isLight
+      ? AppBaseColors.lightTextColors.shade600
+      : AppBaseColors.darkTextColors.shade600;
+
+  Color get textColor700 => isLight
+      ? AppBaseColors.lightTextColors.shade700
+      : AppBaseColors.darkTextColors.shade700;
+
+  Color get textColor800 => isLight
+      ? AppBaseColors.lightTextColors.shade800
+      : AppBaseColors.darkTextColors.shade800;
+
+  Color get errorColor => isLight
+      ? AppBaseColors.errorColor.shade300
+      : AppBaseColors.errorColor.shade200;
+
+  Color get successColor => isLight
+      ? AppBaseColors.successColor.shade300
+      : AppBaseColors.successColor.shade200;
+
+  Color get warningColor => isLight
+      ? AppBaseColors.warningColor.shade300
+      : AppBaseColors.warningColor.shade200;
+}
